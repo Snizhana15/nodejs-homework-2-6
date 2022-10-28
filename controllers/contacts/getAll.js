2368daf4e8610eff85d31b4f1eef0e35a3190ae9
Snizhana15 <snischana15.05@gmail.com>
2022-10-28 17:02:56 +0300

    authorization

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,7 +2,10 @@ const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res, next) => {
   try {
-    const result = await Contact.find({});
+    const { _id: owner } = req.user;
+    const { page = 1, limit = 20 } = req.query;
+    const skip = (page - 1) * limit;
+    const result = await Contact.find({ owner }, "-__v", { skip, limit });
     res.status(200).json(result);
   } catch (error) {
     next(error);
